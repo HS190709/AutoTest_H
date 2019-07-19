01e2c44861e39d1cf0bd42a38039d71d92915150
hosni <hosni@zhangmen.com>
2019-07-19 11:32:40 +0800

    修改关于testng的maven pom文件

--- src/test/java/com/hosni/autotest/chapterfive/Config.java
+++ src/test/java/com/hosni/autotest/chapterfive/Config.java
@@ -2,11 +2,13 @@ package com.hosni.autotest.chapterfive;
 
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
 /**
  * @author hosni
  * @date 2019/07/12 19:53:33
  **/
+@Test
 public class Config {
     @BeforeSuite
     public static void BeforeSuite() {
--- src/test/java/com/hosni/autotest/chapterfive/DependenceTest.java
+++ src/test/java/com/hosni/autotest/chapterfive/DependenceTest.java
@@ -6,6 +6,14 @@ import org.testng.annotations.Test;
  * @author hosni
  * @date 2019/07/18 22:22:22
  **/
-@Test
+
 public class DependenceTest {
+    @Test(dependsOnMethods = {"dependence2"})
+    public static void dependence1(){
+        System.out.println("这是方法1");
+    }
+    @Test
+    public static void dependence2(){
+        System.out.println("这是方法2");
+    }
 }
